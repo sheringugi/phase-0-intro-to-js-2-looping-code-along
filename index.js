ca383f9ad0b9aad7d2b644e797f0a444e0dadebe
sheringugi <sheri.ngugi17@gmail.com>
2023-03-08 13:17:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(message,event) {
+    const messages = []
+    for(let i = 0; i < message.length;i++) {
+        
+           const greetingMessage = `Thank you, ${message[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages;
+
+}
+
+  
+  function countDown (integer) {
+  while(integer>=0){
+        console.log(integer);
+        integer--;
+  }
+}
